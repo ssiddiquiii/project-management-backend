65ba2ff12898e0ee9970b9d0ff00ed3bfdb2f9d8
Sameed Siddiqui <sameedsiddiqui9@gmail.com>
2026-03-10 23:45:47 +0500

    fix bug of async-handler

--- src/controllers/heathcheck.controllers.js
+++ src/controllers/heathcheck.controllers.js
@@ -1,11 +1,9 @@
 import { ApiResponse } from "../utils/api-response.js";
+import { asyncHandler } from "../utils/async-handler.js";
 
-const healthCheck = (req, res) => {
-  try {
-    res
-      .status(200)
-      .json(new ApiResponse(200, { message: "Server is running" }));
-  } catch (error) {}
-};
+// health check function
+const healthCheck = asyncHandler(async (req, res) => {
+  res.status(200).json(new ApiResponse(200, { message: "server is running." }));
+})
 
 export { healthCheck };
